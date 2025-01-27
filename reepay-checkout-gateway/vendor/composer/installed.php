7a2e79358deff6c3c20449a2be1ee60abbd52e86
Sean Murphy <sean@iamseanmurphy.com>
2025-01-27 04:18:35 +0000

    1.7.12.1

--- reepay-checkout-gateway/vendor/composer/installed.php
+++ reepay-checkout-gateway/vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'reepay/checkout',
-        'pretty_version' => '1.7.12',
-        'version' => '1.7.12.0',
+        'pretty_version' => '1.7.12.1',
+        'version' => '1.7.12.1',
         'reference' => null,
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'reepay/checkout' => array(
-            'pretty_version' => '1.7.12',
-            'version' => '1.7.12.0',
+            'pretty_version' => '1.7.12.1',
+            'version' => '1.7.12.1',
             'reference' => null,
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
